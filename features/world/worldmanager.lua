da6364dd67fd4ab20059078862a611e9c7246b99
{jack dunne} <{jack.dunne@orderit.ie}>
2025-03-21 16:58:36 +0000

    detect enemies

--- features/world/worldmanager.lua
+++ features/world/worldmanager.lua
@@ -1,4 +1,4 @@
-
+require("table")
 require("core/utility")
 ---@class WorldManager
 ---@field friendly NPC[]
@@ -22,6 +22,39 @@ function WorldManager:update(dt)
     for _, npc in ipairs(self.enemy) do
         npc:update(dt)
     end
+end
+
+function WorldManager:detectEnemies(npc)
+    local table=npc.friendly and  self.enemy or self.friendly
+    local detectedEnemies={}
+    for _, enemy in ipairs(table) do
+        local x1,x2,y1,y2
+        x1=enemy.hitBoxX
+        x2=enemy.hitBoxX+enemy.hitBoxWidth
+        y1=enemy.hitBoxY
+        y2=enemy.hitBoxY+enemy.hitBoxHeight
+        local intersects=false
+        if LinesIntersect(npc.attackArea, LineSegment.new(x1,y1,x1,y2)) then
+            intersects=true
+           
+         else if LinesIntersect(npc.attackArea, LineSegment.new(x1,y2,x2,y2)) then
+            intersects=true
+        
+         else if LinesIntersect(npc.attackArea, LineSegment.new(x2,y2,x2,y1)) then
+            intersects=true
+          
+         else if LinesIntersect(npc.attackArea, LineSegment.new(x1,y1,x2,y1)) then
+            intersects=true
+       
+        end
+        end
+        end
+        end
+        if intersects then
+            detectedEnemies[#detectedEnemies + 1] = enemy
+        end
+    end
+    return detectedEnemies
 end
 
 function WorldManager:attack(npc)
